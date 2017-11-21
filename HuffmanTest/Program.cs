7af6d4769442a08f1cb8547cca3905ad12f30866
reniasa <reniasaC@gmail.com>
2017-11-21 23:55:56 +0100

    Add files

---
+++ HuffmanTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace HuffmanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a word to encode and decode: ");
+            var input = Console.ReadLine();
+            Tree huffmanTree = new Tree();
+
+            huffmanTree.Build(input);
+
+            BitArray encoded = huffmanTree.Encode(input);
+
+            Console.Write("Encoded: ");
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+
+            string decoded = huffmanTree.Decode(encoded);
+
+            Console.WriteLine("Decoded: " + decoded);
+
+            Console.ReadLine();
+        }
+    }
+}
